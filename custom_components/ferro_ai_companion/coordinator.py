870a47133cc43985430150cdd95ab0ff72beb191
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2025-05-29 15:06:32 +0000

    Correction of five minute trigger.

--- custom_components/ferro_ai_companion/coordinator.py
+++ custom_components/ferro_ai_companion/coordinator.py
@@ -159,6 +159,7 @@ class FerroAICompanionCoordinator:
                 hass,
                 self.update_every_five_minutes,
                 minute=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
+                second=0,
             )
         )
         # Listen for changes to the device.
